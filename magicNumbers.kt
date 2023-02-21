384110ddb2a56025ad89f63250e22ff709e76db4
Marflor98 <100226529+Marflor98@users.noreply.github.com>
2023-02-21 10:50:23 -0600

    Update magicNumbers.kt

--- magicNumbers.kt
+++ magicNumbers.kt
@@ -1,1 +1,10 @@
-
+fun main(args: Array<String>) {
+    print("Enter a month in numeric form, a two digit day, and two digit year: ")
+    val month = 
+    val day =
+    val year =
+    println(name)
+  when () 
+    is String -> println("The date is magic")
+    else -> println("The date is not magic")
+}
