9f0f28c6bf6ad166bc752e18acb9515c84baf445
Marflor98 <100226529+Marflor98@users.noreply.github.com>
2023-02-21 13:04:31 -0600

    Update massWeight.kt

--- massWeight.kt
+++ massWeight.kt
@@ -1,12 +1,11 @@
-fun main(args: Array<String>) {
-    print("Enter an objects mass: ")
-    var mass = readLine()!!
-    var massInNewtons = 9.8 * Mass
-if (massInNewtons > 1,000) {
-        println("The object is too heavy")
-  if (massInNewtons < 10) {
-    println("The object is too light")
-   
+fun readLine() {
+
 }
 
-
+fun main(args: Array<String>) {
+    print("Enter an objects mass: ")
+    val mass = readLine()
+    val massInNewtons = 9.8 * mass
+    if ('massInNewtons > 1,000') {
+        println("The object is too heavy")
+        if ('massInNewtons < 10') println("The object is too light")
