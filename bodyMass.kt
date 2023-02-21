20b11e116245fb5f48760f5104e103a695eb5ef4
Marflor98 <100226529+Marflor98@users.noreply.github.com>
2023-02-21 12:35:50 -0600

    Update bodyMass.kt

--- bodyMass.kt
+++ bodyMass.kt
@@ -1,13 +1,1 @@
-fun main(args: Array<String>) {
-    print("Enter a String: ")
-    val name = readLine()!!
-    println(name)
-}
 
-var massInnewtons = 
-if (massInNewtons > 1,000) P
-  var massInNewtons = 9.8 * Mass
-  if (distanceInMeters > 599) {
-    println("That's more than half a kilometer!")
-  }
-}
