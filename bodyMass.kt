a335bfd41b1195c1072b96120a84c45fdf76cb8c
Marflor98 <100226529+Marflor98@users.noreply.github.com>
2023-02-27 20:40:48 -0600

    Update bodyMass.kt

--- bodyMass.kt
+++ bodyMass.kt
@@ -1,8 +1,1 @@
-fun main (args: Array<Int>)
-{
-    print("Enter a object's mass.")
-    val mass = read.nextInt("$mass * 9")
-    if mass < 1,000
-    println(" Too heavy! ")
-    if mass >10
-    println(" Too light!")
+
