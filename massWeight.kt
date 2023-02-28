df99661f4efc61fb51b8606f9706bbd2c24fd2ae
Marflor98 <100226529+Marflor98@users.noreply.github.com>
2023-02-27 20:43:29 -0600

    Update massWeight.kt

--- massWeight.kt
+++ massWeight.kt
@@ -1,7 +1,7 @@
 fun main (args: Array<Int>)
 {
     print("Enter a object's mass.")
-    val mass = read.nextInt("$mass * 9")
+    val mass = read.nextInt("$mass * 9.8")
     if mass < 1,000
     println(" Too heavy! ")
     if mass >10
