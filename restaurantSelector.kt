8d0af7ba7416386659f2312d4b578bb8d7bd9cb5
Marflor98 <100226529+Marflor98@users.noreply.github.com>
2023-03-01 11:33:51 -0600

    Update restaurantSelector.kt

--- restaurantSelector.kt
+++ restaurantSelector.kt
@@ -1,1 +1,15 @@
-
+fun main(args:Array<string>)
+{
+  println("Is anyone in your party vegetarian?");
+  println(" Is anyone in your party vegan?");
+  println(" Is anyone in your party gluten free?");
+  var answer: readLine()
+  if (answer = yes)
+  println("Here are your restaurant choices:
+Corner Cafe
+The Chef's Kitchen")
+  else -> println("Here are your restaurant choices:
+Main Street Pizza Company
+Corner Cafe
+The Chef's Kitchen")
+}
