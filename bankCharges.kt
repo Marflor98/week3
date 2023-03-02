80cb0d7856a92e41b463c84999feca9a5a790dce
Marflor98 <100226529+Marflor98@users.noreply.github.com>
2023-03-02 09:57:57 -0600

    Update bankCharges.kt

--- bankCharges.kt
+++ bankCharges.kt
@@ -1,1 +1,16 @@
+fun main() {
+    print("Enter number of checks written this month ")
 
+    var numchecks: String =readLine()!!;
+    when
+            (numchecks){ is 1...20
+        ->println("Your bank’s service fees for the month are $10.10")};
+    when
+            (numchecks) { is 20...39
+        ->println("Your bank's service fees for the month are $10.08")};
+    when
+            (numchecks) { is 40...59
+        ->println("Your bank's service fees for the month are $10.06");
+            }
+    when (numchecks) { is 60...
+        -> println("Your bank's service fees for the month are $10.04")}}
